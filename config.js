0db120ba63cb48b3de500b41cc6ee33a89a5b178
Elizabeth Rosso <lizprogrammer@yahoo.com>
2018-10-20 18:54:17 -0400

    twitter bot

---
+++ config.js
@@ -0,0 +1,13 @@
+//config.js
+/** TWITTER APP CONFIGURATION
+ * consumer_key
+ * consumer_secret
+ * access_token
+ * access_token_secret
+ */
+module.exports = {
+  consumer_key: 'SPXwZSR0IYNZQxbSVwCOnAcxh',  
+  consumer_secret: 'VPVR7SJzDG6sDT1SetHppVewKOvPTpHOicgJtsNBQpoSQUv4kG',
+  access_token: '1382501455-IiIuigg98LMZCiWLc2X4UGOfcNl0iOeJmmtaJ1R',  
+  access_token_secret: 'NQxB7MVjtLirhgOt7a0DRSwMhmfgmUWtIR0LHyZThXznh'
+}
